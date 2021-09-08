bfc3252047de2c066d57fd1f130838f9521d1bb7
NguyenDangKhoa02 <90308938+NguyenDangKhoa02@users.noreply.github.com>
2021-09-08 17:49:03 +0700

    .

---
+++ Baitap03/src/main/java/com/mycompany/baitap03/ComputeExpression.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.baitap03;
+
+/**
+ *
+ * @author Admin
+ */
+public class ComputeExpression { 
+	public static void main(String[] args) { 
+		System.out.print("(10.5 + 2 * 3) / (45 – 3.5) = ");
+		System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+	}
+}
---
+++ Baitap03/src/main/java/com/mycompany/baitap03/WelcomeWithThreeMessages.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.baitap03;
+
+/**
+ *
+ * @author Admin
+ */
+public class WelcomeWithThreeMessages { 
+	public static void main(String[] args) { 
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First"); 
+		System.out.println("Problem Driven");
+	}
+}
